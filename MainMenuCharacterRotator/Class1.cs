64324896108132a601943b30eaac7e07a0c87cca
Archie the Inventor <56054349+cheesenibbles123@users.noreply.github.com>
2020-12-07 18:45:10 +0000

    Update Class1.cs

--- MainMenuCharacterRotator/Class1.cs
+++ MainMenuCharacterRotator/Class1.cs
@@ -22,28 +22,29 @@ namespace MainMenuCharacterRotator
         void Update()
         {
             // If it has been found
-            if (menuCharacter)
+            if (!óèïòòåææäêï.åìçæçìíäåóë.activeSelf && global::Input.GetMouseButton(1) && menuCharacter)
             {
-                // Rotate on RMB hold
-                if (global::Input.GetMouseButton(1))
-                {
-                    // Rotation code copied from CharacterCustomizationUI
-                    menuCharacter.Rotate(Vector3.up, 1000f * Time.deltaTime * -global::Input.GetAxisRaw("Mouse X"));
-                }
+                // Rotation code copied from CharacterCustomizationUI
+                menuCharacter.Rotate(Vector3.up, 1000f * Time.deltaTime * -global::Input.GetAxisRaw("Mouse X"));
+
             }
         }
 
         static void setMenuCharacter()
         {
+            // Find the musket object
             var musket = GameObject.Find("wpn_standardMusket_LOD1");
             if (musket != null)
             {
+                // If it exists, then go to root and find the character model in the heirachy
                 Transform rootTransf = musket.transform.root;
                 foreach (Transform transform in rootTransf)
                 {
                     if (transform.name == "default_character_rig")
                     {
+                        // Save it for the rotating in Update()
                         menuCharacter = transform;
+                        break;
                     }
                 }
             }
